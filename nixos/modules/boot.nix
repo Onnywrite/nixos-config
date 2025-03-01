8234e68b04c88c5127fd79dc6fb8ad38f00c3821
Onnywrite <vlad.mokeev.061611@gmail.com>
2025-03-02 00:18:56 +0300

    add ntfs support

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,4 +1,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # It is needed for my hard drive to work
+  boot.supportedFilesystems = [ "ntfs" ];
 }
