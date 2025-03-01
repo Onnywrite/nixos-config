337392be6566a30d677f587bc83a2d279084ac51
Onnywrite <vlad.mokeev.061611@gmail.com>
2025-03-02 01:22:28 +0300

    add git aliases and safe directory

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -3,5 +3,21 @@
     enable = true;
     userName = "Onnywrite";
     userEmail = "vlad.mokeev.061611@gmail.com";
+
+    aliases = {
+      co = "checkout";
+      s = "status";
+      a = "add";
+      c = "commit";
+      p = "push";
+      rb = "rebase";
+      sp = "stash push";
+      spop = "stash pop";
+      sdrop = "stash drop";
+    };
+
+    extraConfig = {
+      safe.directory = "*";
+    };
   };
 }
